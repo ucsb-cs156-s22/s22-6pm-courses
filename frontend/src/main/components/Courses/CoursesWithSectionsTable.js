5b094093f2d9bb83037ed90ad409a7c96322a7ff
hanzemeng <51301998+hanzemeng@users.noreply.github.com>
2022-05-26 15:23:53 -0700

    a

--- frontend/src/main/components/Courses/CoursesWithSectionsTable.js
+++ frontend/src/main/components/Courses/CoursesWithSectionsTable.js
@@ -14,7 +14,7 @@ export default function CoursesWithSectionsTable({ courses }) {
         
         {
             Header: 'Sections Eneroll Code',
-            accessor: (row, _rowIndex) => row.classSections.enrollCode[1],
+            accessor: (row, _rowIndex) => row.classSections[1].enrollCode,
             id: 'classSections.enrollCode',
         },
         
