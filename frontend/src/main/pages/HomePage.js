08dd817ac66ef2088cbb73d6cf220b2bb3c12f32
Srijit Boinapally <srijit@umail.ucsb.edu>
2022-05-29 23:14:32 -0700

    adding fields to section table

--- frontend/src/main/pages/HomePage.js
+++ frontend/src/main/pages/HomePage.js
@@ -19,7 +19,7 @@ export default function HomePage() {
   });
 
   const onSuccess = (courses) => {
-    setCourseJSON(courses.classsection);
+    setCourseJSON(courses.class);
   };
 
   const mutation = useBackendMutation(
