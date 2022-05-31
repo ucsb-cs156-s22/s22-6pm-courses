58b3a6bec4d2925a9f1eaf731596e9d048f35be3
Srijit Boinapally <srijit@umail.ucsb.edu>
2022-05-30 22:26:19 -0700

    Added fixtures for the secttion table

---
+++ frontend/src/fixtures/sectionFixtures.js
@@ -0,0 +1,33 @@
+const sectionsFixtures = {
+    oneSection: {
+        "quarter": "S22",
+        "courseId": "CHEM 1B",
+        "location": "Chem 1179",
+        "enroll": "291/354",
+        "time": "15:00--15:50 M W F",
+        "instructor": "PRICE S P"
+    },
+    twoSections: [
+        {
+            "quarter": "S22",
+            "courseId": "CHEM 1B",
+            "location": "Chem 1179",
+            "enroll": "291/354",
+            "time": "15:00--15:50 M W F",
+            "instructor": "PRICE S P"
+        },
+        {
+            "quarter": "S22",
+            "courseId": "CHEM 1C",
+            "location": "Chem 1179",
+            "enroll": "259/260",
+            "time": "15:00--15:50 M W F",
+            "instructor": "JOSEPH C A"
+        }
+    ]
+};
+
+
+export { sectionsFixtures };
+
+
