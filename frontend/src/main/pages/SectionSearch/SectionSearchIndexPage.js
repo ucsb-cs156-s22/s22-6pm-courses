e285cfe92b746817f488f6c84f0447a918ccdd64
AadityaChannabasappa <81777074+AadityaChannabasappa@users.noreply.github.com>
2022-05-31 22:34:29 -0700

    Update SectionSearchIndexPage so it is merged properly

--- frontend/src/main/pages/SectionSearch/SectionSearchIndexPage.js
+++ frontend/src/main/pages/SectionSearch/SectionSearchIndexPage.js
@@ -44,19 +44,4 @@ export default function SectionSearchIndexPage() {
     </BasicLayout>
   );
 }
-=======
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-export default function SectionSearchIndexPage() {
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>UCSB Course Section Search Placeholder</h1>
-        <p>
-          This is where the index page will go
-        </p>
-      </div>
-    </BasicLayout>
-  )
-} 
 
