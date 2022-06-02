0046b546b1cf2d7f57b9a813044368ff9eeee170
AadityaChannabasappa <81777074+AadityaChannabasappa@users.noreply.github.com>
2022-06-01 17:32:22 -0700

    Create CourseWithSectionsTable.test.js

---
+++ frontend/src/tests/components/Courses/CourseWithSectionsTable.test.js
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import { newsectionFixtures } from "fixtures/newsectionFixtures";
+import CoursesWithSectionsTable from "main/components/Courses/CoursesWithSectionsTable";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("CourseTable tests", () => {
+  const queryClient = new QueryClient();
+
+  test("renders without crashing for empty table", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <CoursesWithSectionsTable courses={[]} />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+  });
+
+  test("Has the expected column headers and content", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <CoursesWithSectionsTable courses={newsectionFixtures.oneSection} />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    const expectedHeaders = ["Course ID", "Section", "Location", "Enrollment", "Time", "Instructor"];
+    const expectedFields = ["courseInfo.courseId", "section", "location", "enrollment", "time", "instructor"];
+    const testId = "CoursesWithSectionsTable";
+
+    expectedHeaders.forEach((headerText) => {
+        const header = screen.getByText(headerText);
+        expect(header).toBeInTheDocument();
+      });
+
+      expectedFields.forEach((field) => {
+        const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+        expect(header).toBeInTheDocument();
+      });
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-courseInfo.courseId`)).toHaveTextContent("CMPSC 5A");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-section`)).toHaveTextContent("LECTURE");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-location`)).toHaveTextContent("ELLSN 2617");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-enrollment`)).toHaveTextContent("85/90");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-time`)).toHaveTextContent("17:00--18:15 T R");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-instructor`)).toHaveTextContent("SOLIS S W, NOBODY, ANYBODY");
+  });
+
+});
