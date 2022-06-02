1d8a976156cbd9fbe184193437c4c1fe38b7adb1
Kevin Lai <clai@ucsb.edu>
2022-06-02 00:47:58 -0700

    KL - replace placeholder section cart with actual table

--- frontend/src/main/pages/SectionSearch/SectionSearchCartPage.js
+++ frontend/src/main/pages/SectionSearch/SectionSearchCartPage.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
+import SectionsCartTable from 'main/components/Courses/SectionsCartTable';
 
 export default function SectionSearchCartPage() {
+  const { data: aSection, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/sectionscart/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/sectionscart/all" },
+      []
+    );
   return (
     <BasicLayout>
       <div className="pt-2">
         <h5>Welcome to the UCSB Course Sections Cart!</h5>
+        <SectionsCartTable aSection={aSection} />
       </div>
     </BasicLayout>
   );
