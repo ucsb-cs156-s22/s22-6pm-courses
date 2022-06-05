acb831df2af2138ad34a40a71f588711464bdcc3
Sid Majeti <sid@169-231-154-5.wireless.ucsb.edu>
2022-06-04 21:31:09 -0700

    Clean commit of only files changed

--- src/main/java/edu/ucsb/cs156/courses/entities/PersonalCourses.java
+++ src/main/java/edu/ucsb/cs156/courses/entities/PersonalCourses.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
-import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumn; 
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -20,6 +20,7 @@ import lombok.Builder;
 public class PersonalCourses {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  // public String getEnrollCd(){return enrollCd;}
   private long id;
   private long psId;
   private String enrollCd;
--- src/main/java/edu/ucsb/cs156/courses/repositories/PersonalCoursesRepository.java
+++ src/main/java/edu/ucsb/cs156/courses/repositories/PersonalCoursesRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface PersonalCoursesRepository extends CrudRepository<PersonalCourses, Long> {
+    Iterable<PersonalCourses> findAllByPsId(Long psId);
 }
